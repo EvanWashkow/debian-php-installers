0d965198e45c0c83dc23b8603bf2e377c8db1014
EvanWashkow <epwashkow@gmail.com>
2018-06-01 09:54:08 -0700

    Write script to install PHP 7.2

---
+++ install_php_7.2.sh
@@ -0,0 +1,15 @@
+# Add the repository
+sudo add-apt-repository ppa:ondrej/php
+
+# Update the package list
+sudo apt-get update
+
+# Remove PHP 7.0
+sudo apt-get purge php7.*
+
+# Install PHP 7.2 and common submodules
+sudo apt-get install php7.2
+sudo apt-get install php-curl php-mbstring php-xml
+
+# Restart apache
+sudo service apache2 restart
