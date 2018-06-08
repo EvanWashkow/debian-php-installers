2c9f9f89168a829600d9bcf7b9aba03b44726441
EvanWashkow <epwashkow@gmail.com>
2018-06-08 09:37:57 -0700

    Ensure the "add-apt-repository" command is installed

--- shared/add_php_repository.sh
+++ shared/add_php_repository.sh
@@ -4,6 +4,9 @@
 # Adds third-party PHP repository to upgrade to other (newer) versions of PHP
 #
 
+# Ensure the "add-apt-repository" command is installed
+yes | sudo apt install software-properties-common
+
 # Add the repository and refresh the package list
 yes | sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
